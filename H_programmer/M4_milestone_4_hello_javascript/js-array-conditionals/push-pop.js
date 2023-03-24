c61c8a4a00f02e151591d1567f299a7b1fdd42c4
csrafsan <rafsanahmedbd21@gmail.com>
2023-03-24 11:46:07 +0600

    js-array-conditonals 50% done

---
+++ H_programmer/M4_milestone_4_hello_javascript/js-array-conditionals/push-pop.js
@@ -0,0 +1,14 @@
+var numbers = [2,2, 7, 4, 4, 3, 7, 8];
+console.log(numbers);
+
+numbers.push(10);
+
+numbers.push('orange');
+
+console.log(numbers);
+
+var element = numbers.pop();
+
+console.log(numbers);
+
+console.log(element);
